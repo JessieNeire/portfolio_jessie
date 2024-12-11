38ad794ab75f749c1b872197d2711aa0dfaa3fcc
Jéssie Neire <126810934+JessieNeire@users.noreply.github.com>
2024-12-11 17:34:43 -0300

    Add files via upload

---
+++ portfolio/cabecalho.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Portfólio Jéssie Neire</title>
+</head>
+
+<body>
+    <nav class="w3-bar w3-mobile w3-top w3-center w3-black" style="height: 50px;">
+        <a href="index.php" class="item_nav w3-bar-item w3-button w3-left w3-hover-pink"><b>Jéssie Neire da Silva Santos</b></a>
+        <a href="curriculo.php" class="item_nav w3-bar-item w3-button w3-right w3-hover-pink"><b>Contato</b></a>
+        <a href="curriculo.php" class="item_nav w3-bar-item w3-button w3-right w3-hover-pink"><b>Currículo</b></a>
+        <a href="projetos.php" class="item_nav w3-bar-item w3-button w3-right w3-hover-pink"><b>Projetos Acadêmicos</b></a>
+    </nav>
---
+++ portfolio/script.js
@@ -0,0 +1,55 @@
+//Slides projetos web
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " w3-white";
+}
+
+// projetos app
+var slideIndex1 = 1;
+showDivs1(slideIndex1);
+
+function plusDivs1(n) {
+    showDivs1(slideIndex1 += n);
+}
+
+function currentDiv1(n) {
+    showDivs(slideIndex1 = n);
+}
+
+function showDivs1(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides1");
+    var dots = document.getElementsByClassName("demo1");
+    if (n > x.length) { slideIndex1 = 1 }
+    if (n < 1) { slideIndex1 = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" w3-white", "");
+    }
+    x[slideIndex1 - 1].style.display = "block";
+    dots[slideInde1x - 1].className += " w3-white";
+}
